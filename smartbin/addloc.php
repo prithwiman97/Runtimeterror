4a019283779f86fac66cbb2eb59b314543f07b22
Prithwiman Mazumdar <30746565+prithwiman97@users.noreply.github.com>
2020-06-13 15:02:02 +0530

    Add files via upload

--- smartbin/addloc.php
+++ smartbin/addloc.php
@@ -1,1 +1,10 @@
-//To be Implemented
+<?php
+    require("entities.php");
+    require("controllers.php");
+    $name=$_POST['name'];
+    $lat=$_POST['lat'];
+    $lng=$_POST['lng'];
+    $loc=new Location(0,$name,$lat,$lng);
+    $loc->insert();
+    header("location:setupbin.php");
+?>
